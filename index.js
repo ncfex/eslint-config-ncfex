212d0a93de60cb53902f9b6623a633549566988b
ncfex <oguzhanguldibi@gmail.com>
2024-06-30 17:44:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./.eslintrc.cjs')
+const path = require('path');
+
+module.exports = {
+  extends: [
+    path.resolve(__dirname, './rules/es6.js'), // not effective, airbnb overrides
+    path.resolve(__dirname, './rules/variables.js'), // not effective, airbnb overrides
+    'airbnb-base',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {}
+};
