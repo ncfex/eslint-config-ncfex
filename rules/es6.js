dffc8ec6d8830c042510bf2eacbd5015f8895b2b
ncfex <oguzhanguldibi@gmail.com>
2024-06-30 16:54:32 +0300

    add rules folder

---
+++ rules/es6.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      generators: false,
+      objectLiteralDuplicateProperties: false
+    }
+  }
+};
