cd052efecfb379cba22a6a88e43776847611c305
oguzhan <oguzhan@ait.com.tr>
2024-07-01 09:41:49 +0300

    add eslint-config-n

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 module.exports = {
   extends: [
+    'eslint:recommended',
     path.resolve(__dirname, './rules/es6.js'), // not effective, airbnb overrides
     path.resolve(__dirname, './rules/variables.js'), // not effective, airbnb overrides
     'airbnb-base',
+    'plugin:n/recommended',
     'prettier'
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module'
   },
   rules: {}
